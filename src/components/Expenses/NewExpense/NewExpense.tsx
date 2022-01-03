f39369e71af831e2c6b0cc9f1c71a887ca9e1706
gregariousgovind <govind.chauhan@mountblue.io>
2022-01-03 15:55:42 +0530

    3 files modified, 2 added

--- src/components/Expenses/NewExpense/NewExpense.tsx
+++ src/components/Expenses/NewExpense/NewExpense.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import 'NewExpense.scss';
+import './NewExpense.scss';
 import ExpenseForm from './ExpenseForm';
 
-const NewExpense = () => {
+const NewExpense = (props) => {
+  const saveExpenseDataHandeler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random.toString(),
+    };
+    props.onAddExpense(expenseData);
+  };
   return (
     <div className="new-expense">
-      <ExpenseForm />
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandeler} />
     </div>
   );
 };
