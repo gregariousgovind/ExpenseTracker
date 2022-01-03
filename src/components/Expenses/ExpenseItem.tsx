a2479393f49580312478629fea0479be521da9bf
gregariousgovind <govind.chauhan@mountblue.io>
2022-01-03 13:47:48 +0530

    1 file modified, 4 added

--- src/components/Expenses/ExpenseItem.tsx
+++ src/components/Expenses/ExpenseItem.tsx
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.scss';
 
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.data.title);
+  const clickHandler = () => {
+    setTitle('Updated!');
+  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.data.date} />
       <div className="expense-item__description">
-        <h2>{props.data.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.data.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
